0d2f1cb00e70b5951684d18c54483abd79c4b90f
Rayhan <rayhan17258@gmail.com>
2026-06-21 22:25:18 +0600

    Solved 1 CF 900 rated problem

---
+++ Codeforces/A_Codecraft_III.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int TC;
+    cin >> TC;
+    while (TC--) {
+        solve();
+    }
+
+    return 0;
+}

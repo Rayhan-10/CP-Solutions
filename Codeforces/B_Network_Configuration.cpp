035c9d53a7eaecc5953eb38ff07aca103d5c6297
Rayhan <rayhan17258@gmail.com>
2026-07-28 20:43:04 +0600

    Solved 1 CF 900 rated problem

---
+++ Codeforces/B_Network_Configuration.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+
+void solve() {
+    int n, k;
+    cin>>n>>k;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.rbegin(), a.rend());
+
+    cout << a[k - 1] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
